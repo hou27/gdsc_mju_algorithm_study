bebd2de19207bce850302d95ac56a8f440d0af63
hou27 <ataj125@gmail.com>
2023-01-01 18:27:34 +0900

    2225 java - #3
    혼자 힘 x

--- mju_1st/week2/2225/Main.java
+++ mju_1st/week2/2225/Main.java
@@ -13,7 +13,21 @@ public class Main {
     } else if (K == 2) {
       System.out.println(N + 1);
     } else {
+      long[][] dp = new long[N + 1][K + 1];
+      for (int i = 0; i <= N; i++) {
+        dp[i][1] = 1;
+      }
+      for (int i = 0; i <= K; i++) {
+        dp[0][i] = 1;
+      }
 
+      for (int i = 1; i <= N; i++) {
+        for (int j = 2; j <= K; j++) {
+          dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+        }
+      }
+
+      System.out.println(dp[N][K]);
     }
   }
 
