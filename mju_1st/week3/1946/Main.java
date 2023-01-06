25e8faae6502cf40a75d029719deca51141726e9
hou27 <ataj125@gmail.com>
2023-01-06 23:12:38 +0900

    1946 java 시간 초과 해결 - #5
    이중 for문, Arrays.sort으로 시도했던 정렬을
    보다 빠른 Collections.sort로 시도하여 성공

--- mju_1st/week3/1946/Main.java
+++ mju_1st/week3/1946/Main.java
@@ -44,7 +44,87 @@
 
 // }
 
-import java.util.Arrays;
+// import java.util.Arrays;
+// import java.util.Scanner;
+
+// public class Main {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int T = sc.nextInt();
+
+//     for (int i = 0; i < T; i++) {
+//       int N = sc.nextInt();
+//       int[][] rank = new int[N][2];
+//       for (int j = 0; j < N; j++) {
+//         rank[j][0] = sc.nextInt(); // 서류심사 성적 순위
+//         rank[j][1] = sc.nextInt(); // 면접심사 성적 순위
+//       }
+
+//       // 서류심사 성적 순위 기준 오름차순 정렬
+//       Arrays.sort(rank, (a, b) -> a[0] - b[0]);
+
+//       int cnt = 1; // 서류 점수가 가장 높은 사람은 무조건 카운트
+//       int top_interview_rate = rank[0][1]; // 현재까지 가장 높은 순위의 면접 성적
+//       for (int j = 1; j < N; j++) {
+//         if (rank[j][1] < top_interview_rate) { // 서류 점수는 더 낮지만 면접 성적이 더 높은 경우 카운트
+//           top_interview_rate = rank[j][1]; // 카운트와 동시에 가장 높은 순위의 면접 성적 갱신
+//           cnt++;
+//         }
+//       }
+//       System.out.println(cnt);
+//     }
+
+//     sc.close();
+
+//   }
+
+// }
+
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.Comparator;
+// import java.util.Scanner;
+
+// public class Main {
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int T = sc.nextInt();
+
+//     for (int i = 0; i < T; i++) {
+//       int N = sc.nextInt();
+//       ArrayList<int[][]> rank = new ArrayList<>();
+//       for (int j = 0; j < N; j++) {
+//         rank.add(new int[][] { { sc.nextInt() }, { sc.nextInt() } });
+//       }
+
+//       // 서류심사 성적 순위 기준 오름차순 정렬
+//       Collections.sort(rank, new Comparator<int[][]>() {
+//         @Override
+//         public int compare(int[][] o1, int[][] o2) {
+//           return o1[0][0] - o2[0][0];
+//         }
+//       });
+
+//       int cnt = 1; // 서류 점수가 가장 높은 사람은 무조건 카운트
+//       int top_interview_rate = rank.get(0)[1][0]; // 현재까지 가장 높은 순위의 면접 성적
+//       for (int j = 1; j < N; j++) {
+//         if (rank.get(j)[1][0] < top_interview_rate) { // 서류 점수는 더 낮지만 면접 성적이 더 높은 경우 카운트
+//           top_interview_rate = rank.get(j)[1][0]; // 카운트와 동시에 가장 높은 순위의 면접 성적 갱신
+//           cnt++;
+//         }
+//       }
+//       System.out.println(cnt);
+//     }
+
+//     sc.close();
+
+//   }
+
+// }
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
@@ -54,20 +134,25 @@ public class Main {
 
     for (int i = 0; i < T; i++) {
       int N = sc.nextInt();
-      int[][] rank = new int[N][2];
+      ArrayList<int[]> rank = new ArrayList<>();
       for (int j = 0; j < N; j++) {
-        rank[j][0] = sc.nextInt(); // 서류심사 성적 순위
-        rank[j][1] = sc.nextInt(); // 면접심사 성적 순위
+        rank.add(new int[] { sc.nextInt(), sc.nextInt() });
       }
 
       // 서류심사 성적 순위 기준 오름차순 정렬
-      Arrays.sort(rank, (a, b) -> a[0] - b[0]);
+      // Collections.sort(rank, new Comparator<int[]>() {
+      // @Override
+      // public int compare(int[] o1, int[] o2) {
+      // return o1[0] - o2[0];
+      // }
+      // });
+      Collections.sort(rank, (o1, o2) -> o1[0] - o2[0]);
 
       int cnt = 1; // 서류 점수가 가장 높은 사람은 무조건 카운트
-      int top_interview_rate = rank[0][1]; // 현재까지 가장 높은 순위의 면접 성적
+      int top_interview_rate = rank.get(0)[1]; // 현재까지 가장 높은 순위의 면접 성적
       for (int j = 1; j < N; j++) {
-        if (rank[j][1] < top_interview_rate) { // 서류 점수는 더 낮지만 면접 성적이 더 높은 경우 카운트
-          top_interview_rate = rank[j][1]; // 카운트와 동시에 가장 높은 순위의 면접 성적 갱신
+        if (rank.get(j)[1] < top_interview_rate) { // 서류 점수는 더 낮지만 면접 성적이 더 높은 경우 카운트
+          top_interview_rate = rank.get(j)[1]; // 카운트와 동시에 가장 높은 순위의 면접 성적 갱신
           cnt++;
         }
       }
