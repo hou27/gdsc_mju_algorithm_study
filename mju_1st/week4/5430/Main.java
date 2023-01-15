19987b873010146dd374bb4028875be5339d4aa2
hou27 <ataj125@gmail.com>
2023-01-15 21:26:51 +0900

    5430 시간초과 해결 후 틀림 - #7
    시간초과를 string builder로 해결한 후
    33%를 넘어가니까 틀렸다고 채점됨

--- mju_1st/week4/5430/Main.java
+++ mju_1st/week4/5430/Main.java
@@ -79,12 +79,100 @@
 //   }
 // }
 
+// import java.util.LinkedList;
+// import java.util.Scanner;
+
+// public class Main {
+//   static LinkedList<String> list = new LinkedList<String>();
+//   static boolean errFlag = false;
+//   static int n;
+
+//   public static void main(String[] args) {
+//     Scanner sc = new Scanner(System.in);
+//     int T = sc.nextInt(); // 테스트 케이스의 개수
+//     String[] arr = null;
+
+//     for (int i = 0; i < T; i++) {
+//       String p = sc.next(); // 수행할 함수
+//       n = sc.nextInt(); // 배열에 들어있는 수의 개수
+//       String arrString = sc.next(); // 배열 문자열
+
+//       // 배열로 만들기
+//       if (n == 0 && p.contains("D")) {
+//         errFlag = true;
+//       } else if (n == 0) {
+//         System.out.println("[]");
+//         continue;
+//       } else {
+//         arrString = arrString.substring(1, arrString.length() - 1);
+//         arr = arrString.split(",");
+
+//         for (int j = 0; j < arr.length; j++) {
+//           list.add(arr[j]);
+//         }
+
+//         for (int j = 0; j < p.length(); j++) {
+//           if (errFlag) {
+//             break;
+//           }
+//           if (p.charAt(j) == 'R') {
+//             R();
+//           } else if (p.charAt(j) == 'D') {
+//             D();
+//           }
+//         }
+//       }
+
+//       if (errFlag) {
+//         System.out.println("error");
+//         errFlag = !errFlag;
+//       } else {
+//         printArr();
+//       }
+//       list.clear();
+//     }
+//     sc.close();
+//   }
+
+//   // 배열에 있는 수의 순서를 뒤집는 함수
+//   static void R() {
+//     for (int i = 0; i < list.size() / 2; i++) {
+//       String tmp = list.get(i);
+//       list.set(i, list.get(list.size() - 1 - i));
+//       list.set(list.size() - 1 - i, tmp);
+//     }
+//   }
+
+//   // 배열의 첫 번째 수를 버리는 함수
+//   static void D() {
+//     if (list.size() == 0) {
+//       errFlag = true;
+//       return;
+//     }
+//     list.remove(0);
+//   }
+
+//   // 배열 포맷으로 출력
+//   static void printArr() {
+//     System.out.print("[");
+//     if (list.size() != 0) {
+//       for (int i = 0; i < list.size() - 1; i++) {
+//         System.out.print(list.get(i));
+//         System.out.print(",");
+//       }
+//       System.out.print(list.get(list.size() - 1));
+//     }
+//     System.out.println("]");
+//   }
+// }
+
+import java.util.Deque;
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-  static LinkedList<String> list = new LinkedList<String>();
-  static boolean errFlag = false;
+  static Deque<String> deque = new LinkedList<String>();
+  static boolean errFlag = false, tailFirst = false;
   static int n;
 
   public static void main(String[] args) {
@@ -99,7 +187,8 @@ public class Main {
 
       // 배열로 만들기
       if (n == 0 && p.contains("D")) {
-        errFlag = true;
+        System.out.println("error");
+        continue;
       } else if (n == 0) {
         System.out.println("[]");
         continue;
@@ -108,7 +197,7 @@ public class Main {
         arr = arrString.split(",");
 
         for (int j = 0; j < arr.length; j++) {
-          list.add(arr[j]);
+          deque.add(arr[j]);
         }
 
         for (int j = 0; j < p.length(); j++) {
@@ -129,39 +218,48 @@ public class Main {
       } else {
         printArr();
       }
-      list.clear();
+      tailFirst = false;
     }
     sc.close();
   }
 
   // 배열에 있는 수의 순서를 뒤집는 함수
   static void R() {
-    for (int i = 0; i < list.size() / 2; i++) {
-      String tmp = list.get(i);
-      list.set(i, list.get(list.size() - 1 - i));
-      list.set(list.size() - 1 - i, tmp);
-    }
+    tailFirst = !tailFirst;
   }
 
   // 배열의 첫 번째 수를 버리는 함수
   static void D() {
-    if (list.size() == 0) {
+    if (deque.isEmpty()) {
       errFlag = true;
       return;
     }
-    list.remove(0);
+    if (tailFirst) {
+      deque.removeLast();
+    } else {
+      deque.removeFirst();
+    }
   }
 
   // 배열 포맷으로 출력
   static void printArr() {
-    System.out.print("[");
-    if (list.size() != 0) {
-      for (int i = 0; i < list.size() - 1; i++) {
-        System.out.print(list.get(i));
-        System.out.print(",");
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
+    if (!deque.isEmpty()) {
+      if (tailFirst) {
+        while (deque.size() > 1) {
+          sb.append(deque.removeLast() + ",");
+        }
+        sb.append(deque.removeLast());
+      } else {
+        while (deque.size() > 1) {
+          sb.append(deque.removeFirst() + ",");
+        }
+        sb.append(deque.removeFirst());
       }
-      System.out.print(list.get(list.size() - 1));
     }
-    System.out.println("]");
+    sb.append("]\n");
+    System.out.println("here");
+    System.out.print(sb);
   }
 }
