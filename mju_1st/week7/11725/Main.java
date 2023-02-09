3295b29336984741d372803961a33b4e9a4cf749
hou27 <ataj125@gmail.com>
2023-02-10 00:38:27 +0900

    11725 java 정보 처리 시기 변경 후 성공 - #12
    노드 간 연결정보를 입력받을 때 정보를 처리하지 않고
    후에 루트 노드부터 훑어내려가며 처리하여 성공함

--- mju_1st/week7/11725/Main.java
+++ mju_1st/week7/11725/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -7,10 +8,12 @@ public class Main {
     int idx;
     Node parent;
     ArrayList<Node> childs;
+    ArrayList<Integer> nodeInfos;
 
     public Node(int idx) {
       this.idx = idx;
       this.childs = new ArrayList<Node>();
+      this.nodeInfos = new ArrayList<Integer>();
     }
 
     public void setParent(Node parent) {
@@ -20,12 +23,15 @@ public class Main {
     public void addChild(Node child) {
       this.childs.add(child);
     }
+
+    public void addNodeInfo(Integer nodeInfo) {
+      this.nodeInfos.add(nodeInfo);
+    }
   }
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int N = sc.nextInt(); // 노드의 개수
-    int[][] nodeInfo = new int[N - 1][2];
     ArrayList<Node> nodes = new ArrayList<Node>();
     for (int i = 1; i < N + 1; i++) {
       nodes.add(new Node(i));
@@ -35,35 +41,48 @@ public class Main {
       int n1 = sc.nextInt();
       int n2 = sc.nextInt();
 
-      nodeInfo[i] = new int[] { n1, n2 };
-    }
-
-    // nodeInfo 정렬
-    Arrays.sort(nodeInfo, (a, b) -> {
-      if (a[0] == b[0]) {
-        return a[1] - b[1];
-      } else {
-        return a[0] - b[0];
-      }
-    });
-
-    for (int i = 0; i < N - 1; i++) {
-      int n1 = nodeInfo[i][0];
-      int n2 = nodeInfo[i][1];
-
       Node node1 = nodes.get(n1 - 1);
       Node node2 = nodes.get(n2 - 1);
 
-      if (node1.parent == null && n1 != 1) {
-        node1.setParent(node2);
-        node2.addChild(node1);
-      } else {
-        node2.setParent(node1);
-        node1.addChild(node2);
-      }
+      if (n2 != 1)
+        node1.addNodeInfo(n2);
+      if (n1 != 1)
+        node2.addNodeInfo(n1);
     }
 
     sc.close();
+
+    Queue<Node> queue = new LinkedList<Node>();
+    Boolean[] visited = new Boolean[N + 1];
+    for (int i = 0; i < N + 1; i++) {
+      visited[i] = false;
+    }
+
+    // 1번 노드부터 있는 것부터 처리
+    for (int i = 0; i < nodes.get(0).nodeInfos.size(); i++) {
+      int nodeIdx = nodes.get(0).nodeInfos.get(i) - 1;
+      Node child = nodes.get(nodeIdx);
+      nodes.get(0).addChild(child);
+      queue.add(child);
+      child.setParent(nodes.get(0));
+      visited[nodeIdx] = true;
+    }
+
+    while (!queue.isEmpty()) {
+      Node node = queue.poll();
+
+      // 자식 노드들 설정 후 큐에 추가
+      for (int i = 0; i < node.nodeInfos.size(); i++) {
+        int nodeIdx = node.nodeInfos.get(i) - 1;
+        if (!visited[nodeIdx]) {
+          Node child = nodes.get(nodeIdx);
+          node.addChild(child);
+          child.setParent(node);
+          queue.add(child);
+          visited[nodeIdx] = true;
+        }
+      }
+    }
 
     for (int i = 1; i < N; i++) {
       System.out.println(nodes.get(i).parent.idx);
